ddb4f0de12362b5f9aff1f7a6476f088ce3ab436
adam <32176844+adamkouhel@users.noreply.github.com>
2017-10-19 10:38:53 -0500

    Initial Commit

---
+++ DelegateMayhem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DelegateMayhem
+//
+//  Created by Adam Kouhel on 10/19/17.
+//  Copyright © 2017 Adam Kouhel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
